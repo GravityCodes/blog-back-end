8e5d17e0ce090bb1e7d7c18a71a89f2c3f72cb29
Johan Mesa <Johanotr@gmail.com>
2025-04-12 18:08:31 -0400

    feat: add user routes and validation

---
+++ routes/user.js
@@ -0,0 +1,13 @@
+const {Router} = require('express');
+const controller = require('../controller/user');
+const validator = require('../validator/user');
+const route = Router();
+
+
+// route.get('/', controller.fetchUser);
+route.post('/', validator.createUser, controller.createUser);
+route.post('/login', validator.loginUser, controller.loginUser);
+// route.delete('/', controller.deleteUser);
+// route.put('/', constroller.updateUser );
+
+module.exports = route;
---
+++ validator/user.js
@@ -0,0 +1,31 @@
+const {body} = require('express-validator');
+
+const createUser = [
+  body('email')
+  .notEmpty()
+  .withMessage("email must not be empty")
+  .isEmail()
+  .withMessage("email has incorrect format"),
+  body('password')
+  .notEmpty()
+  .withMessage('password must not be empty'),
+  body('name')
+  .notEmpty()
+  .withMessage('name must not be empty')
+];
+
+const loginUser = [
+  body('email')
+  .notEmpty()
+  .withMessage("email must not be empty")
+  .isEmail()
+  .withMessage("email has incorrect format"),
+  body('password')
+  .notEmpty()
+  .withMessage('password must not be empty'),
+]
+
+module.exports = {
+  createUser,
+  loginUser
+}
