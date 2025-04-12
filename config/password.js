20121f74ee927e4ffe92b4f4faead36aeead023c
Johan Mesa <Johanotr@gmail.com>
2025-04-12 18:08:07 -0400

    feat: setup password manager

---
+++ config/password.js
@@ -0,0 +1,31 @@
+const bcrypt = require('bcrypt');
+
+const hashPassword = async (password) => {
+  try{
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    return hashedPassword;
+
+  }catch(err){
+    console.error(err);
+    return res.status(500).json("A server error has occured. Please try again later.");
+  };
+
+}
+
+const checkPassword = async (password, hash) => {
+  try {
+    const result = await bcrypt.compare(password, hash);
+
+    return result;
+  }catch(err){
+    console.error(err);
+    return res.status(500).json("A server error has occured. Please try again later.");
+  }
+}
+
+module.exports = {
+  hashPassword,
+  checkPassword
+}
